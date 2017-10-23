e4c45359e17d3f7c9992733f4ab760495f6924ec
liuhailong <liuhailong@55haitao.com>
2017-10-23 11:44:47 +0800

    MD5加密工具

--- src/main/java/cn/sh/util/md5/MD5Demo.java
+++ src/main/java/cn/sh/util/md5/MD5Demo.java
@@ -8,11 +8,9 @@ public class MD5Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
-			String password=MD5Util.getEncryptedPwd("123456");
+			String password=MD5Util.getMD5("123456","");
 			System.out.println(password);
-			boolean result=MD5Util.validPassword("1234562", "88F97B73BC9ECB95D53EA1E2A53D1C5F04581BDBF0C23E26B8A432CA");
-			System.out.println(result);
-			System.out.println(password.length());
+			
 		} catch (NoSuchAlgorithmException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- src/main/java/cn/sh/util/md5/MD5Util.java
+++ src/main/java/cn/sh/util/md5/MD5Util.java
@@ -2,12 +2,9 @@
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.util.Arrays;
 
 public class MD5Util {
 	private static final String HEX_NUMS_STR = "0123456789ABCDEF";
-	private static final Integer SALT_LENGTH = 12;
 
 	/**
 	 * 将16进制字符串转换成字节数组
@@ -44,82 +41,47 @@ public class MD5Util {
 		}
 		return hexString.toString();
 	}
-
+	
 	/**
-	 * 验证口令是否合法
-	 * 
-	 * @param password
-	 * @param passwordInDb
-	 * @return
-	 * @throws NoSuchAlgorithmException
-	 * @throws UnsupportedEncodingException
-	 */
-	public static boolean validPassword(String password, String passwordInDb)
-			throws NoSuchAlgorithmException, UnsupportedEncodingException {
-		// 将16进制字符串格式口令转换成字节数组
-		byte[] pwdInDb = hexStringToByte(passwordInDb);
-		// 声明盐变量
-		byte[] salt = new byte[SALT_LENGTH];
-		// 将盐从数据库中保存的口令字节数组中提取出来
-		System.arraycopy(pwdInDb, 0, salt, 0, SALT_LENGTH);
-		// 创建消息摘要对象
-		MessageDigest md = MessageDigest.getInstance("MD5");
-		// 将盐数据传入消息摘要对象
-		md.update(salt);
-		// 将口令的数据传给消息摘要对象
-		md.update(password.getBytes("UTF-8"));
-		// 生成输入口令的消息摘要
-		byte[] digest = md.digest();
-		// 声明一个保存数据库中口令消息摘要的变量
-		byte[] digestInDb = new byte[pwdInDb.length - SALT_LENGTH];
-		// 取得数据库中口令的消息摘要
-		System.arraycopy(pwdInDb, SALT_LENGTH, digestInDb, 0, digestInDb.length);
-		// 比较根据输入口令生成的消息摘要和数据库中消息摘要是否相同
-		if (Arrays.equals(digest, digestInDb)) {
-			// 口令正确返回口令匹配消息
-			return true;
-		} else {
-			// 口令不正确返回口令不匹配消息
-			return false;
-		}
-	}
-
-	/**
-	 * 获得加密后的16进制形式口令
+	 * 获得加密后的16进制形式口令,口令长度为32位
 	 * 
 	 * @param password
 	 * @return
 	 * @throws NoSuchAlgorithmException
 	 * @throws UnsupportedEncodingException
 	 */
-	public static String getEncryptedPwd(String password)
+	public static String getMD5(String password,String saltString)
 		throws NoSuchAlgorithmException,
 		UnsupportedEncodingException {
-		//声明加密后的口令数组变量
-		byte[] pwd = null;
-		//随机数生成器
-		SecureRandom random = new SecureRandom();
-		//声明盐数组变量
-		byte[] salt = new byte[SALT_LENGTH];
-		//将随机数放入盐变量中
-		random.nextBytes(salt);
+		StringBuffer buffer=new StringBuffer();
+		buffer.append(password);
+		if(saltString!=null||"".equals(saltString)) {
+			buffer.append(saltString);
+		}
 		//声明消息摘要对象
 		MessageDigest md = null;
 		//创建消息摘要
 		md = MessageDigest.getInstance("MD5");
-		//将盐数据传入消息摘要对象
-		md.update(salt);
 		//将口令的数据传给消息摘要对象
-		md.update(password.getBytes("UTF-8"));
+		md.update((buffer.toString()).getBytes("UTF-8"));
 		//获得消息摘要的字节数组
 		byte[] digest = md.digest();
-		//因为要在口令的字节数组中存放盐，所以加上盐的字节长度
-		pwd = new byte[digest.length + SALT_LENGTH];
-		//将盐的字节拷贝到生成的加密口令字节数组的前12个字节，以便在验证口令时
-		System.arraycopy(salt, 0, pwd, 0, SALT_LENGTH);
-		//将消息摘要拷贝到加密口令字节数组从第13个字节开始的字节
-		System.arraycopy(digest, 0, pwd, SALT_LENGTH, digest.length);
 		//将字节数组格式加密后的口令转化为16进制字符串格式的口令
-		return byteToHexString(pwd);
-		}
+		return byteToHexString(digest);
+	}
+	
+	/**
+	 * 获得加密后的16进制形式口令,口令长度为32位
+	 * 
+	 * @param password
+	 * @return
+	 * @throws NoSuchAlgorithmException
+	 * @throws UnsupportedEncodingException
+	 */
+	public static String getMD5(String password)
+		throws NoSuchAlgorithmException,
+		UnsupportedEncodingException {
+		return getMD5(password,null);
+	}
+	
 }
